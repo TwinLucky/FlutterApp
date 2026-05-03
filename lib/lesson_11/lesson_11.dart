0ba00fcd8256aea782dc5966af8c205c34af5765
Bor_ka <razorenovboris@gmail.com>
2026-05-03 23:16:36 +0300

    feat(Flutter-11): Work with Column

--- lib/lesson_11/lesson_11.dart
+++ lib/lesson_11/lesson_11.dart
@@ -11,37 +11,107 @@ class LessonEleven extends StatelessWidget {
         backgroundColor: Colors.blue.shade100,
       ),
       body: Center(
-        child: Container(
-          width: 300,
-          height: 150,
-          alignment: Alignment.bottomRight,
-          padding: const EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            color: const Color.fromRGBO(9, 110, 234, 1),
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            spacing: 10,
-            children: [
-              Image.asset(
-                'assets/images/star.png',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 8,
+          children: [
+            Container(
+              width: 300,
+              height: 150,
+              alignment: Alignment.bottomRight,
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(9, 110, 234, 1),
+                borderRadius: BorderRadius.circular(16),
               ),
-              // const SizedBox(width: 10),
-              Text(
-                'Привіт, Flutter!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                spacing: 10,
+                children: [
+                  Image.asset(
+                    'assets/images/star.png',
+                  ),
+                  // const SizedBox(width: 10),
+                  Text(
+                    'Привіт, Flutter!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  // const SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/star.png',
+                  ),
+                ],
               ),
-              // const SizedBox(width: 10),
-              Image.asset(
-                'assets/images/star.png',
+            ),
+            Container(
+              width: 300,
+              height: 150,
+              alignment: Alignment.bottomRight,
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(8, 233, 0, 1),
+                borderRadius: BorderRadius.circular(16),
               ),
-            ],
-          ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                spacing: 10,
+                children: [
+                  Image.asset(
+                    'assets/images/star.png',
+                  ),
+                  // const SizedBox(width: 10),
+                  Text(
+                    'Привіт, Flutter!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  // const SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/star.png',
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 150,
+              alignment: Alignment.bottomRight,
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(234, 9, 12, 1),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                spacing: 10,
+                children: [
+                  Image.asset(
+                    'assets/images/star.png',
+                  ),
+                  // const SizedBox(width: 10),
+                  Text(
+                    'Привіт, Flutter!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  // const SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/star.png',
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
